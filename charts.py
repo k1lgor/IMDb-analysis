b7737a2198c8ec071cfe998c3a471168b13337e2
Plamen Ivanov <paco.iwanow@gmail.com>
2022-10-13 23:35:00 +0300

    deleted

--- charts.py
+++
@@ -1,8 +0,0 @@
-chart = {
-    1: 'Top 250 Movies',
-    2: 'Most Popular Movies',
-    3: 'Top 250 TV Shows',
-    4: 'Most Popular TV Shows',
-    5: 'Lowest Rated Movies',
-    6: 'Top Rated English Movies'
-}
--- main.py
+++
@@ -1,9 +0,0 @@
-from get_charts import run
-
-
-def main():
-    run()
-
-
-if __name__ == '__main__':
-    main()
