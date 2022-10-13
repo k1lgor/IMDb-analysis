96f6c82bdb3d4402908b23246582a3a169d4eaae
Plamen Ivanov <paco.iwanow@gmail.com>
2022-10-13 23:35:51 +0300

    refactoring

---
+++ IMDb_scraper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from module.get_charts import run
+
+
+def main():
+    run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ module/charts.py
@@ -0,0 +1,8 @@
+chart = {
+    1: 'Top 250 Movies',
+    2: 'Most Popular Movies',
+    3: 'Top 250 TV Shows',
+    4: 'Most Popular TV Shows',
+    5: 'Lowest Rated Movies',
+    6: 'Top Rated English Movies'
+}
